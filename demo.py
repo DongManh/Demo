001fb36899de0e0fdf6512ea3423e86ef0315710
DongManh <161006145+DongManh@users.noreply.github.com>
2024-02-23 14:40:17 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,9 @@
-#Import Streamlit library
 import streamlit as st
-#Displaying Plain Text
-st.write("Hello")
-st.write('World!!!!')
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(
+np.random.randn(30, 10),
+columns=('col_no %d' % i for i in range(10)))
+# defining multiple arguments in write function
+st.write('Here is our Data', df, 'Data is in dataframe
+format.\n', "\nWrite is Super function")
