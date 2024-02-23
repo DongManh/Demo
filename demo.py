442a4026275cec7958a7500ad476e6230e2d2279
DongManh <161006145+DongManh@users.noreply.github.com>
2024-02-23 14:41:15 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
-import streamlit as st
+# importing Necessary Libraries
 import pandas as pd
 import numpy as np
+import altair as alt
+import streamlit as st
+# Defining random Values for Chart
 df = pd.DataFrame(
-np.random.randn(30, 10),
-columns=('col_no %d' % i for i in range(10)))
-# defining multiple arguments in write function
-st.write('Here is our Data', df, 'Data is in dataframe
-format.\n', "\nWrite is Super function")
+np.random.randn(10, 2),
+columns=['a', 'b'])
+# Defining Chart
+chart = alt.Chart(df).mark_bar().encode(
+x='a', y='b', tooltip=['a', 'b'])
+# Defining Chart in write() function
+st.write(chart)
